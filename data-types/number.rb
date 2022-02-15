3aff9127075d412ae51c025ad694abe6ba398bc0
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-15 19:56:24 +0900

    Programe Using Number Variable

---
+++ data-types/number.rb
@@ -0,0 +1,8 @@
+# Data types in RUby represents different types of data like
+# text, string, numbers, etc.
+# All data types are based on classes because it is a pure Object-Oriented Language.
+
+distance = 10
+time = 9.87 / 3600
+speed = distance / time
+puts "The average speed is #{speed} km/h"
