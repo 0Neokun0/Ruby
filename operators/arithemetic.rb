3d93dd31eecbb8b8d813e8c8f62eb548dc908038
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-17 10:00:01 +0900

    Arithemetic Operator Program

---
+++ operators/arithemetic.rb
@@ -0,0 +1,27 @@
+# Ruby Arithmetic Operators
+
+# Addition
+puts ("Addition:")
+puts (10 + 20)
+
+# Subtraction
+puts ("Subtraction:")
+puts (40 - 20)
+
+# Divison
+puts ("Division:")
+puts (100 / 20)
+
+# Multiplication
+puts ("Multiplication:")
+puts (10 * 20)
+
+# Modulus
+puts ("Modulus:")
+puts (20 % 7)
+
+# Exponent
+puts ("Exponent:")
+puts (2 ** 4)
+
+# End
