d6814d919c14b0f010d1ae0b893d69b13d529b21
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-18 10:41:09 +0900

    Ruby If..Else Program

---
+++ if-else/if_else.rb
@@ -0,0 +1,21 @@
+# Ruby if - else - if statement
+
+
+number = 100
+
+if number <= 50
+    puts "Failed"
+
+    elsif number >= 50 && number <= 60
+        puts "You Get D Grade"
+
+    elsif number >= 70 && number <= 80
+        puts "You Get B Grade"
+
+    elsif number >= 80 && number <= 90
+        puts "You Get A Grade"
+
+    elsif number >= 90 && number <= 100
+        puts "You Get A+ Grade, But Then Whats Next ?"
+    end
+
