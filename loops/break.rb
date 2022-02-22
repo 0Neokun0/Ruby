2afbdcac6d89c34e967ff1c8313c38bb07672149
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-22 09:37:34 +0900

    Break Statement Ruby Program

---
+++ loops/break.rb
@@ -0,0 +1,14 @@
+# Ruby Program To Use Break Statement
+
+starting_number = 1
+# Using while
+while true do
+
+    # Printing Value
+    puts starting_number
+    starting_number += 1
+
+    # Using Break Statement
+    break if starting_number > 5
+
+end
