1bac4508e7b11ae791e501790a427150c7072417
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-24 08:35:05 +0900

    Array Ruby Program

---
+++ array/array.rb
@@ -0,0 +1,7 @@
+# Ruby Array Program
+
+# Creating Array of Characters
+array = Array[1,2,3,4,5,6]
+puts "#{array}"
+puts "Size of array is : #{array.size}"
+puts "Length of array is : #{array.length}" 
