ced8e3660813580528be39af3a5d86b61355f7e1
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-25 17:39:05 +0900

    Ruby Methods Program

---
+++ ruby-methods/methods.rb
@@ -0,0 +1,13 @@
+# Ruby Program To Illustrate Method Return Statement
+
+# Method Name
+def number
+    a = 10
+    b = 20
+
+    sum = a + b
+
+    return sum
+end
+
+puts " The Result is : #{number} "
