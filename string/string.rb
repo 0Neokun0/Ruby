595c89ae33f0f4aa4f57b938076e56ed636ead6a
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-27 20:09:40 +0900

    String Ruby Program

---
+++ string/string.rb
@@ -0,0 +1,11 @@
+# Ruby Program To Illustrate The Accessing of String
+
+string = "That Which Is Not Is Called Shiva"
+puts string["That"]
+puts string["Which"]
+
+# Passing Index As An Argument Which Returns The Specified Character
+puts string[3]
+puts string[-3]
+puts string[14, 10]
+puts string[14 .. 17]
