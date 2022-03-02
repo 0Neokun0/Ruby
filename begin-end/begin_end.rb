788f7e735cdd7d09b525614331d34e9726758428
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-02 21:24:04 +0900

    Begin End Method Ruby Program

---
+++ begin-end/begin_end.rb
@@ -0,0 +1,19 @@
+# Ruby Program Of BEGIN and END BLOCK
+
+# Begin Block
+BEGIN {
+    n1 = 4
+    n2 = 3
+    sum= n1 + n2
+    puts "Begin block code"
+    puts sum 
+}
+
+# END block
+END {
+    n1 = 4
+    n2 = 3
+    sum= n1 + n2
+    puts "End block code"
+    puts sum
+}
