99aa6bcdbaf57e027b57d9cf84475fbf110bb4fe
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-07 09:04:16 +0900

    Array Add Ruby Program

---
+++ array/array_add.rb
@@ -0,0 +1,12 @@
+arr = [1, 2]
+puts("Adding 3")
+print (arr + [3])
+puts(" ")
+frameworks = [
+    "React",
+    "Angular",
+    "Vue JS"
+]
+frameworks.insert(2, "Laravel")
+puts("Adding A Framework")
+print(frameworks)
