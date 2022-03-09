acc8d486bd15c411859afbd8d1a1f97b8145e06c
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-09 17:34:47 +0900

    String Join Ruby Program

---
+++ ruby-string-methods/string_join.rb
@@ -0,0 +1,9 @@
+# Ruby Code For Join() Method
+
+# Declaring Array
+sentence1 = ["That", "Which ", "is", "not"]
+sentence2 = ["That", "Which ", "is", "not"]
+
+# Join
+puts "join : #{sentence1.join(" ")}\n\n"
+puts "join : #{sentence2.join(" ")}\n\n"
