b04e102a73e012c2acf6c6762fbe6e3db6412b8e
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-12 18:03:20 +0900

    Reverse Array Ruby Program

---
+++ array/array_reverse.rb
@@ -0,0 +1,8 @@
+# Ruby Code For Reverse() mehtod
+
+# Declaring Array
+array = ["Java", "JavaScript", "Python", "C", "Ruby"]
+
+# Reverse Method Example
+puts "Original Array form : #{array}\n\n"
+puts "Reverse Array form : #{array.reverse()}\n\n"
