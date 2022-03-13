789d2b9838ef0107c1dce5339c0c9facfe6a55e5
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-13 17:34:08 +0900

    Array Pop Ruby Program

---
+++ array/array_pop.rb
@@ -0,0 +1,16 @@
+# Initializing some arrays of elements
+arr = [1, 2, 3, 4, 5, 6, 7]
+  
+# Calling pop() function
+A = arr.pop() 
+B = arr.pop(2)
+C = arr.pop(3)
+D = arr
+  
+# Printing the removed elements 
+puts "#{A}"
+puts "#{B}"
+puts "#{C}"
+  
+# Printing the remaining array
+puts "#{D}"
