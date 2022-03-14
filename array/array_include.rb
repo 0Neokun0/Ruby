4dbc54b9f9eb0b65f05db2ac9106846537f27327
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-14 10:13:14 +0900

    Array Include Method Ruby Program

---
+++ array/array_include.rb
@@ -0,0 +1,10 @@
+# Ruby Program To Demostrate include? method
+
+array = [ "a", "b", "c" ]
+
+print (array)
+puts "\n"
+puts ("Does [b] Exist ?")
+puts (array.include?("b"))
+puts ("Does [z] Exist ?")
+puts (array.include?("z"))
