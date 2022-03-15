0c536fe1520a7a54acc0f848ffc46d253568ffc0
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-15 17:54:22 +0900

    Array Shift

---
+++ array/array_shift.rb
@@ -0,0 +1,9 @@
+# Ruby Code For Shift() Method
+
+# Declaring Array
+array = [ "cat", "dog", "cow"]
+puts "shift() method form : #{array.shift()}\n\n"
+
+# Declaring Array
+array2 = [ "meow", "Bow" , "Mwhaa"]
+puts "shift() method form : #{array2.shift()}\n\n"
