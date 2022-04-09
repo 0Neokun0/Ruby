36c807c63780cda2da27e8ecad7db3976a3afd08
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-09 16:29:49 +0900

    Control Flow Alteration

---
+++ control-flow/control_flow.rb
@@ -0,0 +1,20 @@
+# Ruby program to illustrate break statement
+
+i = 1
+
+# using while loop
+while true
+
+	if i * 6 >= 30
+		
+		# using break statement
+		break
+		
+	# ending of if statement	
+	end
+	
+	puts i * 6
+	i += 1
+	
+# ending of while loop
+end
