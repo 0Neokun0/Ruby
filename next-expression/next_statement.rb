796eccef86696827ccdef22f49f19c96d2c9ee9b
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-10 07:20:12 +0900

    Next Statement

---
+++ next-expression/next_statement.rb
@@ -0,0 +1,14 @@
+# Ruby program of using next statement
+
+for x in 0..6
+
+    # Used condition
+    if x+1 < 4 then
+
+        # Using next statement
+        next
+    end
+
+    # Printing values
+    puts "Value of x is : #{x}"
+end
