5fc4c1e12220981f5b8ab6ce06d7136416581401
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-17 05:55:37 +0900

    Each Iterator

---
+++ iterators/each_iterator.rb
@@ -0,0 +1,25 @@
+# Ruby program to illustrate each iterator
+
+
+# using each iterator
+# here collection is range
+# variable name is i
+(0..9).each do |i|
+	
+	# statement to be executed
+	puts i
+	
+end
+
+a = ['G', 'e', 'e', 'k', 's']
+
+puts "\n"
+
+# using each iterator
+# here collection is an array
+a.each do|arr|
+
+	# statement to be executed
+	puts arr
+	
+end
