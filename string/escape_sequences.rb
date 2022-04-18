ffc5c7b2707a8d3dab89534b7274adac020a2e20
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-19 06:11:47 +0900

    Escape Sequences

---
+++ string/escape_sequences.rb
@@ -0,0 +1,8 @@
+# Ruby program of sting Interpolation
+puts 'guardians\nGroot';
+
+# gets executes and prints Groot on a newline.
+puts "guardians\nGroot";
+
+# takes care of control characters.
+puts "hela\nweds\tpuri";
