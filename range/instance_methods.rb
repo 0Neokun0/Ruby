53ba4a37f1aa0a6b85d1da6311466d94c2315762
0Neokun0 <nishant.meher.9595@gmail.com>
2022-05-10 07:22:49 +0900

    Instance Method

---
+++ range/instance_methods.rb
@@ -0,0 +1,9 @@
+# Ruby program to illustrate
+# the use of == method
+
+# Using Range.new class method
+a = Range.new(2, 5, false)
+b = Range.new(2, 5, false)
+
+# Using == instance method
+a == b				
